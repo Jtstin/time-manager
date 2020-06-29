866b2369059de495cd418305014e993b4e582b3b
Justin Tan <justin@192-168-1-5.tpgi.com.au>
2020-06-29 11:58:25 +1000

    added task list

--- time-manager-ui/Tasks.tsx
+++ time-manager-ui/Tasks.tsx
@@ -1,6 +1,30 @@
 import React from "react";
 
+enum Priority {
+  High = "high",
+  Medium = "medium",
+  Low = "low",
+} //fixed values for priorities
+
+interface Task {
+  name: string;
+  dueBy: string;
+  priority: Priority;
+}
+
 const Tasks = () => {
+  const tasks: Task[] = [
+    {
+      name: "Task 1",
+      dueBy: "01/01/2001",
+      priority: Priority.High,
+    },
+    {
+      name: "Task 2",
+      dueBy: "02/02/2002",
+      priority: Priority.Medium,
+    },
+  ];
   return (
     <div className="task-page">
       <div className="task-container">
@@ -13,7 +37,18 @@ const Tasks = () => {
           </select>
           <button>Search</button>
         </div>
-        <div className="task-list"></div>
+        <div className="task-list">
+          {tasks.map((
+            task,
+            index //transforms each task into their own individual divs to display
+          ) => (
+            <div id={`task-${index}`}>
+              <div>{task.name}</div>
+              <div></div>
+              <div></div>
+            </div>
+          ))}
+        </div>
         <div className="task-add">
           <input type="text"></input>
           <input type="date"></input>
