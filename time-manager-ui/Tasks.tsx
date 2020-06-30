bfefaa6fbcd9ecc551454827ced93035a0df9bc1
Justin Tan <justin@192-168-1-5.tpgi.com.au>
2020-06-30 15:27:59 +1000

    completed majority of task screen

--- time-manager-ui/Tasks.tsx
+++ time-manager-ui/Tasks.tsx
@@ -31,43 +31,57 @@ const Tasks = () => {
     history.push("/schedule");
   };
   return (
-    <div className="task-page">
-      <button onClick={handleScheduleButtonClick}>Schedule </button>
-      <div className="task-container">
-        <div className="task-filter">
-          <input type="text"></input>
-          <select>
-            <option value="High">High</option>
-            <option value="Medium">Medium</option>
-            <option value="Low">Low</option>
-          </select>
-          <button>Search</button>
+    <div className="main-container">
+      <div className="task-page">
+        <div className="task-page-header">
+          <button onClick={handleScheduleButtonClick}>Schedule </button>
         </div>
-        <div className="task-list">
-          {tasks.map((
-            task,
-            index //transforms each task into their own individual divs to display
-          ) => (
-            <div id={`task-${index}`}>
-              <div>{task.name}</div>
-              <div></div>
-              <div></div>
+        <div className="task-page-content">
+          <div className="task-container">
+            <div className="task-filter">
+              <input type="text"></input>
+              <select>
+                <option value="High">High</option>
+                <option value="Medium">Medium</option>
+                <option value="Low">Low</option>
+              </select>
+              <button>Search</button>
             </div>
-          ))}
+            <div className="task-list">
+              {tasks.map((
+                task,
+                index //transforms each task into their own individual divs to display
+              ) => (
+                <div id={`task-${index}`}>
+                  <div>{task.name}</div>
+                  <div></div>
+                  <div></div>
+                </div>
+              ))}
+            </div>
+            <div className="task-add">
+              <input type="text"></input>
+              <input type="date"></input>
+              <select>
+                <option value="High">High</option>
+                <option value="Medium">Medium</option>
+                <option value="Low">Low</option>
+              </select>
+              <button>Add</button>
+            </div>
+          </div>
+          <div className="graph-container">
+            <div className="graph-header">
+              <div></div>
+              <div>COMPLETED TASKS</div>
+              <select className="graph-selector">
+                <option value="Pie">Pie</option>
+                <option value="Bar">Bar</option>
+              </select>
+            </div>
+            <div className="graph-section"></div>
+          </div>
         </div>
-        <div className="task-add">
-          <input type="text"></input>
-          <input type="date"></input>
-          <select>
-            <option value="High">High</option>
-            <option value="Medium">Medium</option>
-            <option value="Low">Low</option>
-          </select>
-          <button>Add</button>
-        </div>
-      </div>
-      <div className="graph-container">
-        <div>right</div>
       </div>
     </div>
   );
