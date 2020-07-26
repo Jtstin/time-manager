59f942797d1cded8822b17cabdc6a59fe64ea54c
Justin Tan <justin@192-168-1-5.tpgi.com.au>
2020-07-26 20:40:37 +1000

    added validation for events

--- time-manager-ui/src/NewEvent.tsx
+++ time-manager-ui/src/NewEvent.tsx
@@ -12,41 +12,77 @@ export function NewEvent(props: NewEventProps) {
   const [name, setName] = useState("");
   const [timeStart, setTimeStart] = useState("");
   const [timeEnd, setTimeEnd] = useState("");
-
+  const [nameErrMsg, setNameErrMsg] = useState("");
+  const [timeEndErrMsg, setTimeEndErrMsg] = useState("");
+  const [timeStartErrMsg, setTimeStartErrMsg] = useState("");
+  const handleSaveEvent = () => {
+    let hasValidationError = false;
+    if (name === "") {
+      setNameErrMsg("Cannot be empty");
+      hasValidationError = true;
+    }
+    if (timeStart === "") {
+      setTimeStartErrMsg("Cannot be empty");
+      hasValidationError = true;
+    }
+    if (timeEnd === "") {
+      setTimeEndErrMsg("Cannot be empty");
+      hasValidationError = true;
+    }
+    if (hasValidationError) {
+      return;
+    }
+    props.handleSaveEvent({
+      id: generateId(),
+      name,
+      timeStart,
+      timeEnd,
+    });
+  };
   return (
     <div className="modify-events">
-      <div>
+      <div className="new-event-name">
         <input
           type="text"
           placeholder="name"
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => {
+            setName(e.target.value);
+            setNameErrMsg("");
+          }}
         ></input>
+        <div className="error-message">{nameErrMsg}</div>
       </div>
-      <div>
+      <div className="new-event-time">
         <input
           type="time"
-          onChange={(e) => setTimeStart(e.target.value)}
+          onChange={(e) => {
+            setTimeStart(e.target.value);
+            setTimeStartErrMsg("");
+          }}
         ></input>
+        <div className="error-message">{timeStartErrMsg}</div>
+      </div>
+      <div className="new-event-time">
+        <input
+          type="time"
+          onChange={(e) => {
+            setTimeEnd(e.target.value);
+            setTimeEndErrMsg("");
+          }}
+        ></input>
+        <div className="error-message">{timeEndErrMsg}</div>
       </div>
       <div>
-        <input type="time" onChange={(e) => setTimeEnd(e.target.value)}></input>
+        <div className="modify-events-button">
+          <button onClick={handleSaveEvent}>Add</button>
+        </div>
+        <div className="error-message"></div>
       </div>
       <div>
-        <button
-          onClick={() =>
-            props.handleSaveEvent({
-              id: generateId(),
-              name,
-              timeStart,
-              timeEnd,
-            })
-          }
-        >
-          Add
-        </button>
-      </div>
-      <div>
-        <button>Edit</button>
+        <div className="modify-event-button">
+          <button>Edit</button>
+        </div>
+        <div className="error-message"></div>
       </div>
     </div>
   );
