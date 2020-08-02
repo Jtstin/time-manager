39cc7221d7cca2817151fcb21ca8714208299f75
Justin Tan <justin@192-168-1-14.tpgi.com.au>
2020-08-02 19:49:57 +1000

    added exceeded passsword tries

--- time-manager-ui/src/Login.tsx
+++ time-manager-ui/src/Login.tsx
@@ -7,14 +7,20 @@ import { api } from "./api";
 export default function Login() {
   const [password, setPassword] = useState("");
   const [loginErrMsg, setLoginErrMsg] = useState("");
+  const [loginFailures, setLoginFailures] = useState(0);
   const history = useHistory();
 
   const handleSaveToken = () => {
+    if (loginFailures > 3) {
+      setLoginErrMsg("Exceeded password tries");
+      return;
+    }
     encrypt(password)
       .then((encryptedPassword) => api.login(encryptedPassword))
       .then((token) => {
         if (token === null) {
           setLoginErrMsg("Incorrect Password");
+          setLoginFailures(loginFailures + 1);
           return;
         }
         saveAccessToken(token);
@@ -25,18 +31,21 @@ export default function Login() {
     <div className="login-page">
       <div className="message">WELCOME BACK</div>
       <div className="password">
-        <label htmlFor="pw">password:</label>
-        <input
-          type="password"
-          id="pw"
-          name="pw"
-          onChange={(e) => {
-            setPassword(e.target.value);
-            setLoginErrMsg("");
-          }}
-        ></input>
-        <div>{loginErrMsg}</div>
-        <button onClick={handleSaveToken}>Login</button>
+        <div>
+          <input
+            placeholder="enter password"
+            type="password"
+            id="pw"
+            name="pw"
+            onChange={(e) => {
+              setPassword(e.target.value);
+              setLoginErrMsg("");
+            }}
+          ></input>
+          <button onClick={handleSaveToken}>Login</button>
+
+          <div className="error-message">{loginErrMsg}</div>
+        </div>
       </div>
     </div>
   );
